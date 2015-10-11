e7bf2a0536fae0f0b4a40ad3a7604821ee21a33b
Bimal Sahay <bimal.sahay@getinsured.com>
2015-10-11 11:01:57 -0700

    Removed user service

--- web/src/main/java/com/bahikhaata/db/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.bahikhaata.db.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserService {
-
-	UserDetails findByUserName(String userName);
-}
